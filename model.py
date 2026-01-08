6912f3bea963503bbd101d7c61008359b0117d62
julia <j49391119@gmail.com>
2026-01-08 20:03:29 -0300

    2nd prototype, trying to make it look more like the actual model

--- model.py
+++ model.py
@@ -3,90 +3,80 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import numpy as np
-from scipy.signal import fftconvolve
 import colorsys
-
-N = 120
-alpha = 0.04
-beta = 0.02
-gamma = 0.02
-delta = 0.01
-sigma = 6.0
-omega = 0.2
-eta = 1.5
-hover_radius = 6.0
-
-x = np.linspace(-1, 1, N)
-y = np.linspace(-1, 1, N)
-X, Y = np.meshgrid(x, y)
-
-A = np.ones((N, N))
-phi = np.random.uniform(0, 2 * np.pi, (N, N))
-psi = A * np.exp(1j * phi)
-
-R = np.sqrt(X**2 + Y**2)
-kernel = np.exp(-(R**2)/(2*sigma**2)) * np.exp(1j * omega * R)
-
-def laplacian(Z):
-    return (
-        np.roll(Z, 1, 0)
-        + np.roll(Z, -1, 0)
-        + np.roll(Z, 1, 1)
-        + np.roll(Z, -1, 1)
-        - 4 * Z
-    )
+import math
+import time
 
 pygame.init()
 display = (1920, 1080)
 pygame.display.set_mode(display, DOUBLEBUF | OPENGL | FULLSCREEN)
-gluPerspective(45, display[0]/display[1], 0.1, 100.0)
-glTranslatef(0, 0, -3)
-glEnable(GL_DEPTH_TEST)
-glPointSize(2)
+gluPerspective(60, display[0] / display[1], 0.1, 1000)
+glTranslatef(0, 0, -140)
+glEnable(GL_BLEND)
+glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-mouse_x = None
-mouse_y = None
+N = 16000
+t = np.linspace(0, 120 * np.pi, N)
 
-clock = pygame.time.Clock()
+observer_theta = 0.0
+observer_phi = 0.0
+observer_bias = 0.0
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-        if event.type == KEYDOWN and event.key == K_ESCAPE:
-            running = False
-        if event.type == MOUSEMOTION:
-            mx, my = event.pos
-            mouse_x = int(mx / display[0] * N)
-            mouse_y = int(my / display[1] * N)
+phase = 0.0
+speed = 0.008
 
-    phi = phi + alpha * laplacian(phi) + beta * np.sin(phi)
-    A = A + gamma * laplacian(A) - delta * (A - 1)
+points = []
 
-    if mouse_x is not None and mouse_y is not None:
-        dx = np.arange(N)[:, None] - mouse_y
-        dy = np.arange(N)[None, :] - mouse_x
-        D = np.sqrt(dx*dx + dy*dy)
-        phi += eta * np.exp(-(D**2)/(2*hover_radius**2))
+def field(phi, time_shift):
+    return (
+        np.sin(2.0 * phi + time_shift)
+        + np.cos(3.1 * phi - time_shift * 0.7)
+        + np.sin(0.9 * phi + np.cos(time_shift))
+    )
 
-    psi = A * np.exp(1j * phi)
-    holo = fftconvolve(psi, kernel, mode="same")
-    I = np.abs(holo)
-    I /= I.max()
+def decode():
+    points.clear()
+    global phase
+    phase += speed
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glBegin(GL_POINTS)
+    psi = field(t + observer_theta, phase)
+    chi = field(t + observer_phi, -phase * 0.6)
 
     for i in range(N):
-        for j in range(N):
-            h = (phi[i, j] % (2*np.pi)) / (2*np.pi)
-            r, g, b = colorsys.hsv_to_rgb(h, 1, I[i, j])
-            glColor3f(r, g, b)
-            glVertex3f(X[i, j], Y[i, j], I[i, j] * 0.8)
+        r = 30 + 14 * psi[i]
+        x = r * math.cos(t[i] + observer_theta + observer_bias)
+        y = r * math.sin(t[i] + observer_phi)
+        z = chi[i] * 50
+        points.append((x, y, z, i / N))
 
+def draw():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glBegin(GL_POINTS)
+    for x, y, z, h in points:
+        r, g, b = colorsys.hsv_to_rgb(h, 1, 1)
+        glColor4f(r, g, b, 0.85)
+        glVertex3f(x, y, z)
     glEnd()
     pygame.display.flip()
-    clock.tick(60)
 
-pygame.quit()
+clock = pygame.time.Clock()
+pygame.event.set_grab(True)
+pygame.mouse.set_visible(False)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            quit()
+        if event.type == KEYDOWN and event.key == K_ESCAPE:
+            pygame.quit()
+            quit()
+        if event.type == MOUSEMOTION:
+            mx, my = event.rel
+            observer_theta += mx * 0.0015
+            observer_phi += my * 0.0015
+            observer_bias += mx * 0.0003
+
+    decode()
+    draw()
+    clock.tick(60)
